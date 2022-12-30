99fc15a4fae70227ce4013131c164fe14f2122d5
hewliyang <hewliyang@u.nus.edu>
2022-12-31 04:06:53 +0800

    Add vercel

--- astro.config.ts
+++ astro.config.ts
@@ -6,25 +6,24 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://www.hewliyang.tech",
-	markdown: {
-		shikiConfig: {
-			theme: "one-dark-pro",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		sitemap(),
-		prefetch(),
-	],
+  site: "https://www.hewliyang.tech",
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true
+    }
+  },
+  integrations: [mdx({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), prefetch()],
+  output: "server",
+  adapter: vercel()
 });
