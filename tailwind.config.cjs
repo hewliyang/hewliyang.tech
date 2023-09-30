6cd07b9ad94996c308b5b19d951a3d2b5afc81bd
hewliyang <hewliyang@u.nus.edu>
2023-09-30 23:13:28 +0800

    [bug] back to pnpm

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -93,7 +93,6 @@ module.exports = {
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
-		require("@tailwindcss/line-clamp"),
 		require("@tailwindcss/aspect-ratio"),
 		plugin(function ({ addComponents }) {
 			addComponents({
